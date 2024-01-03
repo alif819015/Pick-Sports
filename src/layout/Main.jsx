51ccd18d8ee1a541171d30837e45fd09c56d0954
Mahmud Hasan <mahmudhassan11752@gmail.com>
2024-01-03 19:24:23 +0600

    add footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../shared/navBar/NavBar";
+import Footer from "../shared/footer/Footer";
 
 const Main = () => {
  
@@ -7,6 +8,7 @@ const Main = () => {
     <div>
       <NavBar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
